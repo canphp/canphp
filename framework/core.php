9be597a75886e11ac74ec16c621c364f8b7db351
canphp <404352772@qq.com>
2015-01-07 19:54:03 +0800

    去掉protected目录

--- framework/core.php
+++ framework/core.php
@@ -1,6 +1,6 @@
 <?php	
 if( !defined('ROOT_PATH') ) define('ROOT_PATH', realpath('./').DIRECTORY_SEPARATOR);
-if( !defined('BASE_PATH') ) define('BASE_PATH', realpath('./protected').DIRECTORY_SEPARATOR);
+if( !defined('BASE_PATH') ) define('BASE_PATH', realpath('./').DIRECTORY_SEPARATOR);
 if( !defined('CONFIG_PATH') ) define('CONFIG_PATH', BASE_PATH.'data/config/');
 if( !defined('ROOT_URL') ) define('ROOT_URL',  rtrim(dirname($_SERVER["SCRIPT_NAME"]), '\\/').'/');
 if( !defined('PUBLIC_URL') ) define('PUBLIC_URL', ROOT_URL . 'public/');
