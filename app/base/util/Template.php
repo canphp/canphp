6e42122fa7da6d1ea917f57a806253a1d3368156
duxweb <349865361>
2015-01-11 23:15:20 +0800

    将模板标签单独分离到base模块内方便扩展

---
+++ app/base/util/Template.php
@@ -0,0 +1,69 @@
+<?php
+namespace app\base\util;
+/**
+ * 模板标签类 
+ */
+class Template {
+
+	public function tpl($template) {
+
+        $label = array(         
+                /**variable label
+                    {$name} => <?php echo $name;?>
+                    {$user['name']} => <?php echo $user['name'];?>
+                    {$user.name}    => <?php echo $user['name'];?>
+                */  
+                '/{(\\$[a-zA-Z_]\w*(?:\[[\w\.\"\'\[\]\$]+\])*)}/i' => "<?php echo $1; ?>",
+                '/\$(\w+)\.(\w+)\.(\w+)\.(\w+)/is' => "\$\\1['\\2']['\\3']['\\4']",
+                '/\$(\w+)\.(\w+)\.(\w+)/is' => "\$\\1['\\2']['\\3']",
+                '/\$(\w+)\.(\w+)/is' => "\$\\1['\\2']",
+                
+                /**constance label
+                {CONSTANCE} => <?php echo CONSTANCE;?>
+                */
+                '/\{([A-Z_\x7f-\xff][A-Z0-9_\x7f-\xff]*)\}/s' => "<?php echo \\1;?>",
+                
+                /**include label
+                    {include file="test"}
+                */                              
+                '/{include\s*file=\"(.*)\"}/i' => "<?php \$__Template->compile(\"$1\"); ?>",
+                
+                /**if label
+                    {if $name==1}       =>  <?php if ($name==1){ ?>
+                    {elseif $name==2}   =>  <?php } elseif ($name==2){ ?>
+                    {else}              =>  <?php } else { ?>
+                    {/if}               =>  <?php } ?>
+                */              
+                '/\{if\s+(.+?)\}/' => "<?php if(\\1) { ?>",
+                '/\{else\}/' => "<?php } else { ?>",
+                '/\{elseif\s+(.+?)\}/' => "<?php } elseif (\\1) { ?>",
+                '/\{\/if\}/' => "<?php } ?>",
+                
+                /**for label
+                    {for $i=0;$i<10;$i++}   =>  <?php for($i=0;$i<10;$i++) { ?>
+                    {/for}                  =>  <?php } ?>
+                */              
+                '/\{for\s+(.+?)\}/' => "<?php for(\\1) { ?>",
+                '/\{\/for\}/' => "<?php } ?>",
+                
+                /**foreach label
+                    {foreach $arr as $vo}           =>  <?php $n=1; if (is_array($arr) foreach($arr as $vo){ ?>
+                    {foreach $arr as $key => $vo}   =>  <?php $n=1; if (is_array($array) foreach($arr as $key => $vo){ ?>
+                    {/foreach}                  =>  <?php $n++;}unset($n) ?> 
+                */
+                '/\{foreach\s+(\S+)\s+as\s+(\S+)\}/' => "<?php \$n=1;if(is_array(\\1)) foreach(\\1 as \\2) { ?>", 
+                '/\{foreach\s+(\S+)\s+as\s+(\S+)\s*=>\s*(\S+)\}/' => "<?php \$n=1; if(is_array(\\1)) foreach(\\1 as \\2 => \\3) { ?>",
+                '/\{\/foreach\}/' => "<?php \$n++;}unset(\$n); ?>",
+                
+                /**function label
+                    {date('Y-m-d H:i:s')}   =>  <?php echo date('Y-m-d H:i:s');?> 
+                    {$date('Y-m-d H:i:s')}  =>  <?php echo $date('Y-m-d H:i:s');?> 
+                */
+                '/\{([a-zA-Z_\x7f-\xff][a-zA-Z0-9_\x7f-\xff:]*\(([^{}]*)\))\}/' => "<?php echo \\1;?>",
+                '/\{(\\$[a-zA-Z_\x7f-\xff][a-zA-Z0-9_\x7f-\xff:]*\(([^{}]*)\))\}/' => "<?php echo \\1;?>", 
+        );
+
+        return preg_replace(array_keys($label['reg']), array_values($label['reg']), $template);
+	}
+
+}
--- framework/base/Template.php
+++ framework/base/Template.php
@@ -8,62 +8,6 @@ class Template {
 	public function __construct($config) {
 		$this->config = $config;
 		$this->assign('__Template', $this);
-												
-		$this->_replace['reg'] = array(			
-				/**variable label
-					{$name}	=> <?php echo $name;?>
-					{$user['name']}	=> <?php echo $user['name'];?>
-					{$user.name}	=> <?php echo $user['name'];?>
-				*/	
-				'/{(\\$[a-zA-Z_]\w*(?:\[[\w\.\"\'\[\]\$]+\])*)}/i' => "<?php echo $1; ?>",
-				'/\$(\w+)\.(\w+)\.(\w+)\.(\w+)/is' => "\$\\1['\\2']['\\3']['\\4']",
-				'/\$(\w+)\.(\w+)\.(\w+)/is' => "\$\\1['\\2']['\\3']",
-				'/\$(\w+)\.(\w+)/is' => "\$\\1['\\2']",
-				
-				/**constance label
-				{CONSTANCE}	=> <?php echo CONSTANCE;?>
-				*/
-				'/\{([A-Z_\x7f-\xff][A-Z0-9_\x7f-\xff]*)\}/s' => "<?php echo \\1;?>",
-				
-				/**include label
-					{include file="test"}
-				*/								
-				'/{include\s*file=\"(.*)\"}/i' => "<?php \$__Template->compile(\"$1\"); ?>",
-				
-				/**if label
-					{if $name==1}		=>	<?php if ($name==1){ ?>
-					{elseif $name==2}	=>	<?php } elseif ($name==2){ ?>
-					{else}				=>	<?php } else { ?>
-					{/if}				=>	<?php } ?>
-				*/				
-				'/\{if\s+(.+?)\}/' => "<?php if(\\1) { ?>",
-				'/\{else\}/' => "<?php } else { ?>",
-				'/\{elseif\s+(.+?)\}/' => "<?php } elseif (\\1) { ?>",
-				'/\{\/if\}/' => "<?php } ?>",
-				
-				/**for label
-					{for $i=0;$i<10;$i++}	=>	<?php for($i=0;$i<10;$i++) { ?>
-					{/for}					=>	<?php } ?>
-				*/				
-				'/\{for\s+(.+?)\}/' => "<?php for(\\1) { ?>",
-				'/\{\/for\}/' => "<?php } ?>",
-				
-				/**foreach label
-					{foreach $arr as $vo}			=>	<?php $n=1; if (is_array($arr) foreach($arr as $vo){ ?>
-					{foreach $arr as $key => $vo}	=>	<?php $n=1; if (is_array($array) foreach($arr as $key => $vo){ ?>
-					{/foreach}					=>	<?php $n++;}unset($n) ?> 
-				*/
-				'/\{foreach\s+(\S+)\s+as\s+(\S+)\}/' => "<?php \$n=1;if(is_array(\\1)) foreach(\\1 as \\2) { ?>", 
-				'/\{foreach\s+(\S+)\s+as\s+(\S+)\s*=>\s*(\S+)\}/' => "<?php \$n=1; if(is_array(\\1)) foreach(\\1 as \\2 => \\3) { ?>",
-				'/\{\/foreach\}/' => "<?php \$n++;}unset(\$n); ?>",
-				
-				/**function label
-					{date('Y-m-d H:i:s')}	=>	<?php echo date('Y-m-d H:i:s');?> 
-					{$date('Y-m-d H:i:s')}	=>	<?php echo $date('Y-m-d H:i:s');?> 
-				*/
-				'/\{([a-zA-Z_\x7f-\xff][a-zA-Z0-9_\x7f-\xff:]*\(([^{}]*)\))\}/' => "<?php echo \\1;?>",
-				'/\{(\\$[a-zA-Z_\x7f-\xff][a-zA-Z0-9_\x7f-\xff:]*\(([^{}]*)\))\}/' => "<?php echo \\1;?>", 
-		);
 		$this->cache = new Cache( $this->config['TPL_CACHE'] );
 	}
 	
@@ -117,6 +61,8 @@ class Template {
 			$template = $isTpl ? file_get_contents( $tplFile ) : $tpl;
 			$template = str_replace(array_keys($this->_replace['str']), array_values($this->_replace['str']), $template);
 			$template = preg_replace(array_keys($this->_replace['reg']), array_values($this->_replace['reg']), $template);
+			$extObj = new \app\base\util\Template();
+			$template = $extObj->tpl($template);
 			$ret = array('template'=>$template, 'compile_time'=>time());
 			$this->cache->set( $tplKey, serialize($ret), 86400*365);
 		}	
