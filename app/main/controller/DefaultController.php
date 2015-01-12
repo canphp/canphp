11d242af532dd2a175bdde49b88e659a0f1f14f8
duxweb <349865361>
2015-01-13 00:49:18 +0800

    删除部分无用模块，修正默认模块

--- app/main/controller/DefaultController.php
+++ app/main/controller/DefaultController.php
@@ -5,7 +5,6 @@ class DefaultController extends \app\base\controller\BaseController{
 	public function index(){
 		$this->title = model('Demo')->getTitle();
 		$this->hello = model('Demo')->getHello();
-		model('Demo')->join('a')->find();
 		$this->display();
 	}
 }
--- app/main/model/Demo.php
+++ app/main/model/Demo.php
@@ -2,14 +2,8 @@
 namespace app\main\model;
 class Demo extends \app\base\model\BaseModel{
 	protected $table = 'test';
+	
 	public function getTitle(){
-		//$ret = $this->getFields();
-		//echo $this->data( array('name'=>time()) )->insert(  );
-		//echo $this->data(array('name'=>'16') )->where( array('id'=>16) )->update();
-		//echo $this->data(array('name'=>'16') )->count();
-		//echo $this->getSql();
-		//print_r($ret);
-
 		return '默认首页';
 	}
 	
